3553162df43ec734fd2d97be6d7adf8c7aaf07c6
Muditgargcoder <46025257+Muditgargcoder@users.noreply.github.com>
2019-10-12 22:28:41 +0530

    Update LogIn.java

--- src/project/LogIn.java
+++ src/project/LogIn.java
@@ -144,7 +144,7 @@ public class LogIn extends javax.swing.JFrame {
 
             try {
                 Class.forName("java.sql.Driver");
-                java.sql.Connection con = DriverManager.getConnection("jdbc:mysql://localhost:3306/mudit", "root", "madhu304");
+                java.sql.Connection con = DriverManager.getConnection("jdbc:mysql://localhost:3306/your_database_name", "root", "mysql password");
 
                 Statement stmt = (Statement) con.createStatement();
 
