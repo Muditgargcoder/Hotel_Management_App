03b3a6934f938c3dcd3e381b5761f85f98140238
Muditgargcoder <46025257+Muditgargcoder@users.noreply.github.com>
2019-10-12 22:29:23 +0530

    Update Newcustomer.java

--- src/project/Newcustomer.java
+++ src/project/Newcustomer.java
@@ -323,7 +323,7 @@ public class Newcustomer extends javax.swing.JFrame {
 //SAVE BUTTON
 try {
     Class.forName("java.sql.Driver");
-    java.sql.Connection con = DriverManager.getConnection("jdbc:mysql://localhost:3306/mudit", "root", "madhu304");
+    java.sql.Connection con = DriverManager.getConnection("jdbc:mysql://localhost:3306/your_database_name", "root", "mysql password");
 
     int g, k = 0;
     String rt;//room type
@@ -457,7 +457,7 @@ jButton1.setEnabled(false);
         }
         try {
             Class.forName("java.sql.Driver");
-            java.sql.Connection con = DriverManager.getConnection("jdbc:mysql://localhost:3306/mudit", "root", "madhu304");
+            java.sql.Connection con = DriverManager.getConnection("jdbc:mysql://localhost:3306/your_database_name", "root", "mysql password");
 
             Statement stmt = (Statement) con.createStatement();
 
